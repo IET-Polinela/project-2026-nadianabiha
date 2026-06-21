78f4179d1d5b517db7fe98c0c75a6a5dc10eadd6
nadianabiha <nabihanadia8@gmail.com>
2026-06-22 00:38:05 +0700

    Lab Session 14 – OpenAPI-based Documentation

--- server_smartcity/smartcity_app/urls.py
+++ server_smartcity/smartcity_app/urls.py
@@ -1,5 +1,3 @@
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from drf_spectacular_scalar.views import SpectacularScalarView
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -21,7 +19,4 @@ urlpatterns = [
     path('api/docs/scalar/', scalar_viewer, name='scalar-ui'),
     path("api/auth/", include("usermanagement_24782087.api_urls")),
     path("api/", include("main_app.api_urls")),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/scalar/', SpectacularScalarView.as_view(url_name='schema'), name='scalar'),
 ]
